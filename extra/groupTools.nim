dc71d0b99e3e361101e611ed4e53d9f7d7869fd1
dmknght <dmknght@localhost.localdomain>
2020-10-24 04:40:05 +0700

    add filter and correct categori numb

--- extra/groupTools.nim
+++ extra/groupTools.nim
@@ -37,31 +37,31 @@ proc parser() =
       elif line.startsWith("Categories"):
         categories = line.split("=")[^1]
     for thisCat in categories.split(";"):
-      if thisCat.startsWith("01-"):
+      if thisCat.startsWith("01-") and not contains(seqInfo, pkgName):
         seqInfo.add(pkgName)
-      elif thisCat.startsWith("02-"):
+      elif thisCat.startsWith("02-") and not contains(seqVuln, pkgName):
         seqVuln.add(pkgName)
-      elif thisCat.startsWith("03-"):
+      elif thisCat.startsWith("03-") and not contains(seqWeb, pkgName):
         seqWeb.add(pkgName)
-      elif thisCat.startsWith("04-"):
+      elif thisCat.startsWith("04-") and not contains(seqPwn, pkgName):
         seqPwn.add(pkgName)
-      elif thisCat.startsWith("05-"):
+      elif thisCat.startsWith("10-") and not contains(seqMaintain, pkgName):
         seqMaintain.add(pkgName)
-      elif thisCat.startsWith("06-"):
+      elif thisCat.startsWith("08-") and not contains(seqPost, pkgName):
         seqPost.add(pkgName)
-      elif thisCat.startsWith("07-"):
+      elif thisCat.startsWith("05-") and not contains(seqPwd, pkgName):
         seqPwd.add(pkgName)
-      elif thisCat.startsWith("08-"):
+      elif thisCat.startsWith("06-") and not contains(seqWireless, pkgName):
         seqWireless.add(pkgName)
-      elif thisCat.startsWith("09-"):
+      elif thisCat.startsWith("09-") and not contains(seqSniff, pkgName):
         seqSniff.add(pkgName)
-      elif thisCat.startsWith("10-"):
+      elif thisCat.startsWith("11-") and not contains(seqFor, pkgName):
         seqFor.add(pkgName)
-      elif thisCat.startsWith("11-"):
+      elif thisCat.startsWith("13-") and not contains(seqAuto, pkgName):
         seqAuto.add(pkgName)
-      elif thisCat.startsWith("12-"):
+      elif thisCat.startsWith("07-") and not contains(seqRev, pkgName):
         seqRev.add(pkgName)
-      elif thisCat.startsWith("13-"):
+      elif thisCat.startsWith("12-") and not contains(seqReport, pkgName):
         seqReport.add(pkgName)
   echo "Info"
   for this in seqInfo:
