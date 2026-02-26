7bfdd9a5635a9a7f752bdc042fc00c826a607a3c
Dario Camonita <danterolle@parrotsec.org>
2026-02-26 15:28:55 +0100

    perf: increase scanner buffer

--- launcher-updater/internal/dpkg/dpkg.go
+++ launcher-updater/internal/dpkg/dpkg.go
@@ -11,7 +11,10 @@ import (
 const statusPath = "/var/lib/dpkg/status"
 
 func QueryInstalled() (map[string]struct{}, error) {
+	// Instead of launching `dpkg -l`, let's open and read `/var/lib/dpkg/status`.
+	// If we find "install ok installed" for each package, we add it to a map[string]struct{} for O(1) lookups.
 	installed := make(map[string]struct{})
+
 	file, err := os.Open(statusPath)
 	if err != nil {
 		return nil, fmt.Errorf("could not open dpkg status file: %w", err)
@@ -24,6 +27,11 @@ func QueryInstalled() (map[string]struct{}, error) {
 	}(file)
 
 	scanner := bufio.NewScanner(file)
+
+	const maxCapacity = 1024 * 1024
+	buf := make([]byte, maxCapacity)
+	scanner.Buffer(buf, maxCapacity)
+
 	var pkgName string
 	for scanner.Scan() {
 		line := scanner.Text()
